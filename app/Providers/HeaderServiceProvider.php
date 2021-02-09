346ddc27b5e8f213090afaf9491b701abce99e2b
quietanon <tailornirav01@gmail.com>
2021-02-09 15:07:52 +0530

    update

---
+++ app/Providers/HeaderServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use DB;
+
+class HeaderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer('includes.header', function ($view){
+            if (auth() -> check()) {
+                $current = DB::table('carts')
+                    ->join('products', 'products.id', '=', 'carts.product_id')
+                    ->select('products.name', 'products.finalPrize', 'products.images', 'carts.quantity', 'carts.id', 'carts.product_id')
+                    ->where('user_id', auth()->user()->id)
+                    ->where('active', 'true')
+                    ->get();
+                $count = $current->count();
+                $total = 0;
+                foreach($current as $single) {
+                    $total = $total + ($single->finalPrize * $single->quantity);
+                }
+                $view->with(['headercarts' => $current, 'count' => $count, 'total' => $total]);
+            } else{
+                $view->with(['headercarts' => null, 'count' => 0, 'total' => 0]);
+            }
+        });
+    }
+}
